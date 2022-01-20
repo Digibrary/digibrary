730e98107c143398f6645cc36a596ddea50a0f30
the-devil-reincarnated <98079335+the-devil-reincarnated@users.noreply.github.com>
2022-01-20 16:53:51 +0700

    Update library.ts
    Added function to search for books on http://www.openlibrary.org and Google Books
    Uses Bookcut (Python) and Google-books-cli (Nodejs)

--- routes/library.ts
+++ routes/library.ts
@@ -1,14 +1,58 @@
 /**
- * The library page, not much here yet
- */
+* The library page, not much here yet
+*/
 import * as express from "express";
+import { NotificationType } from "../typing/notification-type";
+import { addMessage } from "../utils/message";
+import utilRoutes from "./utils";
 var router = express.Router();
+import util from 'util';
+const exec = util.promisify(require('child_process').exec);
 
 /**
- * Get the home page
- */
+* Get the home page
+*/
 router.get('/', function(req:express.Request, res: express.Response, next: express.NextFunction) {
-  res.render('library/index', { title: 'Digibrary', session:req.session, notifications: req.session.notifications});
+    res.render('library/index', { title: 'Digibrary' });
 });
+
+/**
+ * Check if the session belongs to an admin, running commands on a server is dangerous after all.
+ */
+router.all('*', utilRoutes.is_admin)
+
+router.get('/search', async function(req:express.Request, res: express.Response, next: express.NextFunction) {
+    var { title,ﾠ} = req.query;
+    if(!title){
+        res.render('library/search', { title: 'Search for books' });
+        return;
+    }
+    
+    title = title.toString(); //explicitly convert title to string type
+    const isValidTitle = /^[A-Za-z0-9\s\-_,\.;:()]+$/.test(title);
+    if(!isValidTitle){
+        addMessage(req, {title: "Error", value: "Book title is not valid!", type:NotificationType.ERROR})
+        res.redirect('/library/search')
+        return;
+    }
+    
+    const cmds = [
+        'bookcut details -b "'+title+'"',
+        '/usr/utils/google-books-cli/bin/books-cli search --keyword "'+title+'"',ﾠ
+    ];
+    
+    var result = ""
+    for (const cmd of cmds) {
+        try{
+            var {stdout, stderr} = await exec(cmd, {timeout: 15000 })
+            result += stdout+stderr
+        }catch(e){
+            
+        }
+    }
+    res.render('library/search', { title: 'Search for books', result: result});
+});
+
+router.post('/search')
 
 export default router;
